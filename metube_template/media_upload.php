54d24cb9ebe5fa0a3840a08c9e21e6f5363a1a5f
Kaleb K Chestnut <46284423+KKchestnut@users.noreply.github.com>
2020-11-10 20:13:48 -0500

    Update media_upload.php
    Changed from 1 MB to 1GB

--- metube_template/media_upload.php
+++ metube_template/media_upload.php
@@ -13,8 +13,8 @@ session_start();
 <form method="post" action="media_upload_process.php" enctype="multipart/form-data" >
  
   <p style="margin:0; padding:0">
-  <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
-   Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
+  <input type="hidden" name="MAX_FILE_SIZE" value="1073741824" />
+   Add a Media: <label style="color:#663399"><em> (Each file limit 1G)</em></label><br/>
    <input  name="file" type="file" size="50" />
   
 	<input value="Upload" name="submit" type="submit" />
