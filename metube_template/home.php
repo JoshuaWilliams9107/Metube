db6d8612320b871a27d9275bd2065a136b0e6492
Kaleb K Chestnut <46284423+KKchestnut@users.noreply.github.com>
2020-11-16 12:26:04 -0500

    Added search bar

--- metube_template/home.php
+++ metube_template/home.php
@@ -13,6 +13,13 @@ if(isset($_POST['logout'])) {
  
 ?>
 <body style="padding:0;margin:0;">
+	<form action="search.php" method="get">
+		<label>
+			Search
+			<input type="text" name="keywords" autocomplete="off">
+		</label>
+		<input type="submit" value="Search">
+	</form>
 	<ul>
 	  <li><a id="floatleft" class="active" href="/home.php">Home</a></li>
 	  <li><a id="floatleft" href="/media.php">Upload Media</a></li>
