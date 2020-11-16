b2230179184494f8aa602152ecc05b439d9910e6
KKchestnut <kchestn@g.clemson.edu>
2020-11-16 14:05:05 -0500

    added some backend searching

--- metube_template/search.php
+++ metube_template/search.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 include_once "function.php";
+$link = $db->db_connect_id;
 
 if(isset($_GET['keywords'])){
-    $keywords = $db->real_escape_string($_GET['keywords']);
+    $keywords = mysqli_real_escape_string($link, $_GET['keywords']);
 
     $query = $db->query("
         SELECT filename
