1626c06a6d58fddaee89b342246467096a308082
KKchestnut <kchestn@g.clemson.edu>
2020-11-17 21:02:47 -0500

    fix css issues

--- metube_template/search.php
+++ metube_template/search.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include_once "function.php";
-<link rel="stylessheet" href="css/bootstrap-4.5.3-dist/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 if(isset($_GET['keywords'])){
     $keywords = mysql_escape_string($_GET['keywords']);
