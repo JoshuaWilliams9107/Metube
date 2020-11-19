b7a640cbe320d8981c6662731092ab8b1914287f
KKchestnut <kchestn@g.clemson.edu>
2020-11-18 19:24:04 -0500

    updated media player for mp4

--- metube_template/media_upload.php
+++ metube_template/media_upload.php
@@ -5,6 +5,7 @@ session_start();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <title>Media Upload</title>
 </head>
 
