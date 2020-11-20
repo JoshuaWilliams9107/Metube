b94319d467df571dc0e771f4972c22e7b17c521b
KKchestnut <kchestn@g.clemson.edu>
2020-11-20 00:28:10 -0500

    adding download functionality

--- metube_template/media_download_process.php
+++ metube_template/media_download_process.php
@@ -11,10 +11,26 @@ include_once "function.php";
 $username=$_SESSION['username'];
 $mediaid=$_REQUEST['id'];
 
-//insert into upload table
+//insert into download table
 $insertDownload="insert into download(downloadid,username,mediaid) values(NULL,'$username','$mediaid')";
 $queryresult = mysql_query($insertDownload)
-	
+    
+$getDirectory ="SELECT filepath FROM media WHERE mediaid = '$mediaid' ";    
+$dirfile = mysql_query($getDirectory);
+$downfile = $dirfile.urlencode($_FILE["file"]["name"]);
+$file = $downfile;
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}	
 ?>
 
 
