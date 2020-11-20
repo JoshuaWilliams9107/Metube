e7c578a376f7c4fde9b3ff4c9aa864c0bda8e0fe
KKchestnut <kchestn@g.clemson.edu>
2020-11-20 00:42:41 -0500

    made uploaded files readable

--- metube_template/media_download_process.php
+++ metube_template/media_download_process.php
@@ -22,14 +22,16 @@ $file = $downfile;
 
 if (file_exists($file)) {
     header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Content-Type: application/force-download');
+    header("Content-Disposition: attachment; filename=\"" . basename($file) . "\";");
+    header('Content-Transfer-Encoding: binary');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($file));
-    flush(); // Flush system output buffer
-    readfile($file);
+    ob_clean();
+    flush();
+    readfile($name); //showing the path to the server where the file is to be download
     exit;
 }	
 ?>
