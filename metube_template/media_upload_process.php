e39778678e10377a39605ae2bb221d67e631c5ba
KKchestnut <kchestn@g.clemson.edu>
2020-11-25 02:14:25 -0500

    experiment

--- metube_template/media_upload_process.php
+++ metube_template/media_upload_process.php
@@ -42,11 +42,11 @@ if(!file_exists($dirfile))
                     			chmod($upfile, 0644);
 					//insert into media table
 
-					$insert = "insert into media(
-							  mediaid,filename,filepath,type,title,description,category)".
-                              "values(NULL,'".urlencode($_FILES["file"]["name"])."','$dirfile','
-                              ".$_FILES["file"]["type"]."','" .$_POST["title"]."','
-                              ".$_POST["description"]."','".$_POST["category"]."')";
+					            $insert = "insert into media(
+							        mediaid,filename,filepath,type,title,description,category)".
+                                    "values(NULL,'".urlencode($_FILES["file"]["name"])."',"$dirfile",'
+                                    ".$_FILES["file"]["type"]."','" .$_POST["title"]."','
+                                    ".$_POST["description"]."','".$_POST["category"]."')";
 					$queryresult = mysql_query($insert)
 						  or die("Insert into Media error in media_upload_process.php " .mysql_error());
 
