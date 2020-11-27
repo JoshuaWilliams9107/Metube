875b71be4205f9116b906b1b418a3811367019c2
Joshua Williams <jjw2@clemson.edu>
2020-11-27 11:21:33 -0500

    Added nav bar to upload media view

--- metube_template/media_upload.php
+++ metube_template/media_upload.php
@@ -6,12 +6,24 @@ include_once "logincheck.php";
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/default.css" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <title>Media Upload</title>
 </head>
 
 <body>
-
+<ul>
+    <li><a id="floatleft" href="./home.php">Home</a></li>
+    <li><a id="floatleft" href="./media.php">Browse Media</a></li>
+    <?php if($_SESSION['username'] != ""){?>
+      <li><a id="floatleft" class="active" href='media_upload.php'>Upload Media</a></li>
+    <li><a id="floatleft" href="./channel.php?username=<?php echo $_SESSION['username']?>">My Channel</a></li>
+    <li><a id="floatleft" href='./inbox.php'>Inbox</a></li>
+    <?php }?>
+    <form action="<?php echo "home.php";?>" method="post">
+        <button id="logout" type="submit" name="logout" value="true" class="btn-link">Logout</button>
+    </form>
+  </ul>
 <form method="post" action="media_upload_process.php" enctype="multipart/form-data" >
  
   <p style="margin:0; padding:0">
