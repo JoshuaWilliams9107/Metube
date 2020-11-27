b609c2eafea91b3f384cbc4d5fd9275e46816cb3
KKchestnut <kchestn@g.clemson.edu>
2020-11-27 18:31:38 -0500

    reverting search to older style

--- metube_template/search.php
+++ metube_template/search.php
@@ -34,57 +34,14 @@ if(isset($_GET['keywords'])){
             $query_file = mysql_fetch_assoc($query);
             $query = array_unique($query_file);
             ?>
-
-        <body style="padding:0, margin:0;">
-        <ul>
-            <li><a id="floatleft" href="./home.php">Home</a></li>
-        <ul>
-        <center>
-        <table style="width:70%">
-        <tr>
-        <?php
-        $rowSize = 3;
-        if(mysql_num_rows($query) == 0){
-            echo "<p style='font-size:20px;'> No videos found </p>";
-        }
-        for($i=0; $i<mysql_num_rows($query); $i++){
-            if($searchinfo = mysql_fetch_row($query)){
-                if($i % $rowSize == 0){
-                    ?>
-                    </tr>
-                    <tr>
-                <?php } ?>
-                <td>
-                    <center>
-                <a href="./media.php?id=<?php echo $searchinfo[0];?>">
-
-                <?php if(strpos($searchinfo[3], 'image') !== false){?>
-                    <img src="<?php echo $searchinfo[2].$searchinfo[1]; ?>"
-                alt="thumbnail" width=250px height=150px/><br>
-                <?php } else if(!is_null($searchinfo[9])){?>
-                <img src="<?php echo $searchinfo[2]."thumbnail/".$searchinfo[9]?>"
-                alt="thumbnail" width=250px height=150px/><br>
-                
-                <?php } else{?>
-                <img src="uploads/metube/BlankVideo.png"
-                alt="blank user image" width=250px height=150px/><br>
-
-                <?php
-                }
-                echo "<p>".$searchinfo[4]."<p>";?>
-                </a>
-                </center>
-                </td>
-            <?php
-            }
-            else{
-                break;
-            }
-        }
+            <div class="result">
+            <?php foreach($query as $r) ?>
+                <a href="#"><?php echo $r; ?> </a>     
+            </div>
+            <?php>
         }
    }
 }
-        ?>
         </tr>
         </table>
         </div>
