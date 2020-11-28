06e859818208049dfffdd248688c4203f4c8870b
KKchestnut <kchestn@g.clemson.edu>
2020-11-28 00:08:09 -0500

    backend favorite.php

--- metube_template/favorites.php
+++ metube_template/favorites.php
@@ -11,4 +11,27 @@ include_once "logincheck.php";
 
 $userID=$_SESSION['username'];
 
-$file=$_GET['filename'];
+$playlist=$_GET['playlist'];
+
+$query = "SELECT playlistid FROM  playlist WHERE playlistname = '".$playlist."'";
+$play = mysql_query($query);
+$play_A = mysql_fetch_assoc($play);
+$play_id = $play_A['playlistid'];
+
+if(isset($_GET['filename']) && !empty($_GET['filename'])){    
+    $check = "SELECT * FROM favorite_table WHERE username = '$userID'";
+    $result = mysql_query($check) or die("Selected from favorite_table" .mysql_error());
+    $data = mysql_fetch_array($result, MYSQL_NUM);
+    if($data[0] > 1){
+        $insertUFR = "UPDATE favorite_table SET playlist_id='".$play_id."' WHERE username='".$userID."')";
+        $queryresult = mysql_query($insertUFR) 
+            or die("Insert into media_to_favorites in favorite.php" .mysql_error());
+        }
+    else{    
+        $insertF = "INSERT into favorite_table(username, playlist_id) VALUES('$userID', '$play_id')";
+        $queryresult = mysql_query($insertF)
+            or die("Insert into favorite_table in favorite.php" .mysql_error());
+        }
+    }
+?>
+
