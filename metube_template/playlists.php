16e46bec24e415c35f2dd5e2484839d6ba9fe346
KKchestnut <kchestn@g.clemson.edu>
2020-11-28 11:02:59 -0500

    backend favorite.php

--- metube_template/playlists.php
+++ metube_template/playlists.php
@@ -29,31 +29,6 @@ if(isset($_POST['createPlaylist'])) {
 	}	
 }
 
-$userID=$_SESSION['username'];
-
-$playlist=$_GET['playlist'];
-
-$query = "SELECT playlistid FROM  playlist WHERE playlistname = '".$playlist."'";
-$play = mysql_query($query);
-$play_A = mysql_fetch_assoc($play);
-$play_id = $play_A['playlistid'];
-
-if(isset($_GET['playlist']) && !empty($_GET['playlist'])){    
-    $check = "SELECT * FROM favorite_table WHERE username = '$userID'";
-    $result = mysql_query($check) or die("Selected from favorite_table" .mysql_error());
-    $data = mysql_fetch_array($result, MYSQL_NUM);
-    if($data[0] >= 1){
-        $insertUFR = "UPDATE favorite_table SET playlist_id='$play_id' WHERE username='$userID'";
-        echo "Updated!";
-        $queryresult = mysql_query($insertUFR) 
-            or die("Update into media_to_favorites in favorite.php" .mysql_error());
-        }
-    else{    
-        $insertF = "INSERT into favorite_table(username, playlist_id) VALUES('$userID', '$play_id')";
-        $queryresult = mysql_query($insertF)
-            or die("Insert into favorite_table in favorite.php" .mysql_error());
-        }
-    }
 
 if(isset($_POST['friendDecision'])) {
 	if($_POST['friendDecision'] == "Accept"){
@@ -124,6 +99,31 @@ if(isset($_POST['friendDecision'])) {
             <br>
 		<?php }
 	}
+$userID=$_SESSION['username'];
+
+$playlist=$_GET['playlist'];
+
+$query = "SELECT playlistid FROM  playlist WHERE playlistname = '".$playlist."'";
+$play = mysql_query($query);
+$play_A = mysql_fetch_assoc($play);
+$play_id = $play_A['playlistid'];
+
+if(isset($_GET['playlist']) && !empty($_GET['playlist'])){    
+    $check = "SELECT * FROM favorite_table WHERE username = '$userID'";
+    $result = mysql_query($check) or die("Selected from favorite_table" .mysql_error());
+    $data = mysql_fetch_array($result, MYSQL_NUM);
+    if($data[0] >= 1){
+        $insertUFR = "UPDATE favorite_table SET playlist_id='$play_id' WHERE username='$userID'";
+        echo "Updated!";
+        $queryresult = mysql_query($insertUFR) 
+            or die("Update into media_to_favorites in favorite.php" .mysql_error());
+        }
+    else{    
+        $insertF = "INSERT into favorite_table(username, playlist_id) VALUES('$userID', '$play_id')";
+        $queryresult = mysql_query($insertF)
+            or die("Insert into favorite_table in favorite.php" .mysql_error());
+        }
+    }
 	?>
 	</center>
 		</div>
