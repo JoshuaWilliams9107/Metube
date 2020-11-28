2ab81b9527a54b42b979366c9ce9a7ec53b36740
KKchestnut <kchestn@g.clemson.edu>
2020-11-27 20:10:12 -0500

    Last time for search

--- metube_template/search.php
+++ metube_template/search.php
@@ -43,7 +43,7 @@ if(isset($_GET['keywords'])){
 
             <div class="result">
             <?php foreach($media_Arr as $r) ?>
-                <a href="#"><?php echo implode(', ', $r); ?> </a>     
+                <a href="#"><?php echo $r; ?> </a>     
             </div>
 <?php
 }
