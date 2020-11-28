0b042ae7c52dd0050c16132256fcf31d46041a83
KKchestnut <kchestn@g.clemson.edu>
2020-11-27 19:45:05 -0500

    added some backend searching

--- metube_template/search.php
+++ metube_template/search.php
@@ -32,8 +32,7 @@ if(isset($_GET['keywords'])){
             WHERE mediaid = '".$id[0]."'");
             ?>
             <?php
-            $query_file = mysql_fetch_assoc($query);
-            $media_Arr[] = array_merge($media_Arr, $query_file);
+            $media_Arr[] = mysql_fetch_array($query);
             ?>
             <?php
         }
@@ -43,7 +42,7 @@ if(isset($_GET['keywords'])){
 
             <div class="result">
             <?php foreach($media_Arr as $r) ?>
-                <a href="#"><?php echo $r[0]; ?> </a>     
+                <a href="#"><?php echo $r; ?> </a>     
             </div>
 <?php
 }
