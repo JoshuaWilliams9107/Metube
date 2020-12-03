847b7e369cfe713d2ab8e9f93a6ecf703fbd346e
KKchestnut <kchestn@g.clemson.edu>
2020-12-03 14:22:14 -0500

    made categories view better

--- metube_template/category.php
+++ metube_template/category.php
@@ -10,9 +10,48 @@ include_once "function.php";
 <body style="padding:0;margin:0;">
     <ul>
         <li><a id="floatleft" href="./home.php">Home</a></li>
-    <ul>
-
+        <li><a id="floatleft" href="./media_upload.php">Upload Media</a></li>
+        <li><a id="floatleft" href="./favoriteview.php">Favorite Playlist</a></li>
+        <li><a id="floatleft" href="./channel.php?username=<?php echo $_SESSION['username']?>">My Channel</a></li>
+        <li><a id="floatleft" href="./inbox.php">Inbox</a></li>
+	    <form action="<?php echo "home.php";?>" method="post">
+   			<button id="logout" type="submit" name="logout" value="true" class="btn-link">Logout</button>
+	    </form>
+    </ul>
+    
+    <div class="result">
+    <?php
+    $media_Arr = array_values(array_unique($media_Arr, SORT_REGULAR));
+    ?>
     <center>
+    <div>
+        <form action="search.php" method="get">
+        <label>
+            Search
+            <input type="text" name="keywords" autocomplete="off" value="<?php echo $_GET['keywords'];?>">
+            </label>
+            <input type="submit" value="Search">
+        </form>
+        <style>
+            select:required:invalid {align-content: center; color: grey;}
+            option[value=""][disabled]{ display: none;}
+            option{ color: black; }
+        </style>
+        <form action="category.php" method="get">
+            <select required id="category" name="category">
+                <option value="" disabled selected>Categories</option>
+                <option value="Sports">Sports</option>
+                <option value="Education">Education</option>
+                <option value="Video Games">Video Games</option>
+                <option value="Vlogs">Vlogs</option>
+                <option value="Podcasts">Podcasts</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Others">Others</option>
+            </select>
+            <input value="Go" name="submit" type="submit"/>
+        </form>
+    </div>
+
     <table style="width:70%">
     <tr>
     <?php
